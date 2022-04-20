7bfc000c0f38d83961012bc608514443d27e5651
Dima Titov <dmitri611@gmail.com>
2022-04-20 17:31:27 +0300

    1_commit

--- REAL_HW_JS_1.js
+++ REAL_HW_JS_1.js
@@ -1,20 +1,25 @@
-let age_1 = prompt("Сколько Вам лет? ", );
-    var age_2 = 18,
-        age_3 = 60
-     
-    if (age_1 < age_2 ) {
-       document.write("you don't have acces cause your age is " + age_1 + ", it is less, than " + age_2)
-    }
-    else if  (age_1 >= age_3) {
-        document.write("keep calm and watch Culture channel")
-    } 
-    else if (age_1 < age_3 && age_1 >= age_2 ) {
-        document.write("you are welcome")
-    }
-    else if (typeof  age_1 === "string"  ){
-        document.write("Only numbers")
-    }
-    
+let age_1 = prompt("Сколько Вам лет? ", ),
+    age_2 = 18,
+    age_3 = 60
+    age_4 = 0
+
+if ((age_1 > age_4 ) == true){ 
+     age_1 = Number (age_1)
+}
+else if (typeof (age_1) === 'string') {
+    document.write("only numbers")
+}
+
+if (age_1 < age_2 ) {
+    document.write("you don't have acces cause your age is " + age_1 + ", it is less, than " + age_2)
+}
+else if  (age_1 >= age_3) {
+    document.write(" keep calm and watch Culture channel")
+} 
+else if (age_1 < age_3 && age_1 >= age_2 ) {
+    document.write("you are welcome")
+}
+
      
      
     
